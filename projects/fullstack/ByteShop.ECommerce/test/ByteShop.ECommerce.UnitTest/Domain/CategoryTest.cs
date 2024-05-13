d273aade05239e328a97fa77986abc6c6fc568ee
Allyson Reis <allysonreis1@gmail.com>
2024-05-13 16:58:24 -0300

    Adding CategoryTest

---
+++ projects/fullstack/ByteShop.ECommerce/test/ByteShop.ECommerce.UnitTest/Domain/CategoryTest.cs
@@ -0,0 +1,19 @@
+﻿using ByteShop.ECommerce.Domain.Entities;
+
+namespace ByteShop.ECommerce.UnitTest.Domain;
+
+public class CategoryTest
+{
+    [Fact]
+    public void ShouldInstantiateCategory()
+    {
+        // Arrange
+        var name = "Category 1";
+
+        // Act
+        var category = new Category(name);
+
+        // Assert
+        Assert.Equal(name, category.Name);
+    }
+}
