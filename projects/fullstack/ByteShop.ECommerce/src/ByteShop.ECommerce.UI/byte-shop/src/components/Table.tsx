e491f22fbad6bc285a18ff05003628c8ae16e4dc
Allyson Reis <allysonreis1@gmail.com>
2024-06-03 20:43:55 -0300

    Add Table component for displaying categories

---
+++ projects/fullstack/ByteShop.ECommerce/src/ByteShop.ECommerce.UI/byte-shop/src/components/Table.tsx
@@ -0,0 +1,44 @@
+import styles from './Table.module.css';
+
+export function Table() {
+    return (
+        <table className={styles.table}>
+            <thead>
+                <tr>
+                    <th><input type="checkbox" /></th>
+                    <th><button>Name</button></th>
+                    <th><button>Created At</button></th>
+                    <th><button>Status</button></th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td><input type="checkbox" /></td>
+                    <td>Eletronics</td>
+                    <td>2021-10-10</td>
+                    <td>Active</td>
+                    <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+                {
+                    [...Array(10)].map((_, i) => (
+                        <tr key={i}>
+                            <td><input type="checkbox" /></td>
+                            <td>Eletronics</td>
+                            <td>2021-10-10</td>
+                            <td>Active</td>
+                            <td>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+    );
+}
