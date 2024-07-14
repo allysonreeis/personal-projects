a56d8536eb87efae2d5f280005f112944fcd9497
Allyson Reis <allysonreis1@gmail.com>
2024-07-13 21:12:03 -0300

    creating html structure for navigation

--- projects/fullstack/ByteShop.ECommerce/src/ByteShop.ECommerce.UI/byte-shop/src/components/Home/index.tsx
+++ projects/fullstack/ByteShop.ECommerce/src/ByteShop.ECommerce.UI/byte-shop/src/components/Home/index.tsx
@@ -2,9 +2,33 @@ import styles from './styles.module.css'
 
 export function Home() {
   return (
-    <>
-      <h1 className={styles.home}>Hello World</h1>
-      <p>Teste de paragrafo</p>
-    </>
+    <section className={styles.home}>
+      <nav>
+        <div>
+          <img
+            src="/src/assets/Dribbble_logo_perple.png"
+            alt="logo"
+            className={styles.logo}
+          />
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Shop</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <input type="text" placeholder="Busque aqui" />
+        <button>Login</button>
+        <button>Sign Up</button>
+      </nav>
+    </section>
   )
 }
