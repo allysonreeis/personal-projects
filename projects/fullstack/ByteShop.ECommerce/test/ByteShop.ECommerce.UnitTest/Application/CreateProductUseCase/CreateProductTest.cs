286a267ed0158131732f62d8406a534cf63a8479
Allyson Reis <allysonreis1@gmail.com>
2024-07-25 20:33:55 -0300

    test for create product usecase

---
+++ projects/fullstack/ByteShop.ECommerce/test/ByteShop.ECommerce.UnitTest/Application/CreateProductUseCase/CreateProductTest.cs
@@ -0,0 +1,28 @@
+﻿using ByteShop.ECommerce.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteShop.ECommerce.UnitTest.Application.CreateProductUseCase;
+public class CreateProductTest
+{
+    [Fact]
+    public async void ShouldCreateProduct()
+    {
+        var repositoryMock = new Mock<IProductRepository>();
+        var createProduct = new CreateProduct(repositoryMock.Object);
+        var createProductInput = new CreateProductInput("Product 1", 10.00, "Description", category, 5);
+        var createProductOutput = await createProduct.Handle(createProductInput, CancellationToken.None);
+
+        repositoryMock.Verify(repository => repository.Insert(It.IsAny<Product>(), It.IsAny<CancellationToken>()), Times.Once);
+
+        Assert.NotNull(createProductOutput);
+        Assert.NotEqual(Guid.Empty, createProductOutput.Id);
+        Assert.Equal(createProductInput.Name, createProductOutput.Name);
+        Assert.Equal(createProductInput.Price, createProductOutput.Price);
+        Assert.Equal(createProductInput.CategoryId, createProductOutput.CategoryId);
+    }
+}
